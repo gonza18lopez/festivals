15a2648eacdd511d8523ab04cc994943bba152af
Gonza Lopez <soygonzalopez@gmail.com>
2021-03-05 20:03:02 -0300

    update tests

--- tests/Feature/TourTest.php
+++ tests/Feature/TourTest.php
@@ -24,35 +24,4 @@ class TourTest extends TestCase
 		$response
 			->assertRedirect();
 	}
-
-	/**
-	 * Check if user can enter to /tour path
-	 */
-	public function test_user_can_enter_tour()
-	{
-		$user = User::factory()->create();
-
-		$response = $this->actingAs($user)->get('/tour');
-
-		$response
-			->assertSee('tour')
-			->assertOk();
-	}
-
-	/**
-	 * Check if user can participate in event
-	 */
-	public function test_user_can_participate()
-	{
-		$user = User::factory()->create();
-		$tour = Festival::factory()->create();
-
-		$response = $this->actingAs($user, 'api')->postJson("/api/tour/{$tour->id}");
-
-		$response
-			->assertCreated()
-			->assertJsonStructure([
-				'status'
-			]);
-	}
 }
